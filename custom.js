f3ca1f5880e1ad12c405446426453da25a29db40
prantikv <prantikv@yahoo.com>
2014-11-22 21:21:16 +0530

    bug fix

--- custom.js
+++ custom.js
@@ -1,10 +1,7 @@
 
-
-
-
 function onInitFs(fs){
 	 fileSystem = fs;
-	 alert("fileSystem->"+fileSystem.name+"path is->"fileSystem.fullPath );
+	 alert("fileSystem->"+fileSystem.name+"path is->"+fileSystem.fullPath );
 	// fileSystem.root.getFile('prantik.txt', {create: true, exclusive: true}, function(fileEntry) {
 
  //    fileEntry.isFile === true
