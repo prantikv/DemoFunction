db5db1dd35a7a35d2a711bd0c1a5f9b6ed9ec56e
prantikv <prantikv@yahoo.com>
2014-11-22 20:29:25 +0530

    functions rearranged

--- custom.js
+++ custom.js
@@ -1,18 +1,26 @@
-function domLoaded(){
-	alert("dom loaded");
 
-document.addEventListener("deviceready", onDeviceReady, false);
+
+
+
+function onInitFs(fs){
+	 fileSystem = fs;
+	 alert("got the file fileSystem" );
+}
+function errorHandler(e){
+	alert("Files system error"+e.code);
 
 }
 
 function onDeviceReady() {
  
  	alert(cordova.file.externalRootDirectory);
-    window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, onFSSuccess, onError);
+    window.requestFileSystem(LocalFileSystem.PERSISTENT, 5*1024*1024 /*5MB*/, onInitFs, errorHandler);
 
 }
 
-function onFSSuccess(fs){
-	 fileSystem = fs;
-	 alert("got the file fileSystem" );
+function domLoaded(){
+	alert("dom loaded");
+
+document.addEventListener("deviceready", onDeviceReady, false);
+
 }
