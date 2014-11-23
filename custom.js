5b648c054c94d74ea82f716ec04b59296229530d
prantikv <prantikv@yahoo.com>
2014-11-23 16:18:52 +0530

    file write example

--- custom.js
+++ custom.js
@@ -1,21 +1,50 @@
 
+var theEntry;
+var fileSystem;
+
+
 function onInitFs(fs){
 	 fileSystem = fs;
 	 alert("fileSystem->"+fileSystem.name+" root  is->"+fileSystem.root );
 	 console.log(fileSystem.root);
-
-	// fileSystem.root.getFile('prantik.txt', {create: true, exclusive: true}, function(fileEntry) {
-
- //    fileEntry.isFile === true
- //     fileEntry.name == 'prantik.txt'
- //     fileEntry.fullPath == '/prantik.txt'
- //     alert("file created");
- //  }, errorHandler);
-
-//}
-
 }
 
+function writeFile() {
+  //Get a file name for the file
+  var theFileName = createRandomString(8) + '.txt';
+  alert("writeFile: " + theFileName);
+  theFileSystem.root.getFile(theFileName, {create : true}, onGetFileSuccess, onFileError);
+}
+
+function onGetFileSuccess(theFile) {
+  alert("onGetFileSuccess: " + theFile.name);
+  theFile.createWriter(onCreateWriterSuccess, errorHandler);
+}
+
+
+
+function onCreateWriterSuccess(writer) {
+  alert("onCreateWriterSuccess");
+
+  writer.onwritestart = function(e) {
+console.log("Write start");
+  };
+
+  writer.onwriteend = function(e) {
+    console.log("Write end");
+  };
+
+  writer.onwrite = function(e) {
+console.log("Write completed");
+  };
+
+  writer.onerror = function(e) {
+    console.log("Write error: " + e.toString() + br);
+  };
+  writer.write("File created by Example 18-1: ");
+  writer.write("This is another line of text ");
+  writer.write(createRandomStory(25));
+}
 
 function errorHandler(e){
 	alert("Files system error"+e.code);
